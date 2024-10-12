1a78263fd000f6b910de63bf289efa622e1ab857
reynaldo0 <reynaldooyusellino@gmail.com>
2024-10-12 17:17:43 +0700

    add : color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100 : "#34D399",
+          200 : "#A7F3D0",
+          300 : "#064E3B",
+        },
+        secondary: {
+          100 : "#EF4444",
+          200 : "#FEE2E2",
+          300 : "#7F1D1D",
+        },
+        normal: {
+          100 : "#F3F4F6",
+          200 : "#9CA3AF",
+          300 : "#1F2937",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
