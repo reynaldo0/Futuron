de6697e7fe5aeeb08ccf69bdc2570265dcef32df
reynaldo0 <reynaldooyusellino@gmail.com>
2024-10-16 13:23:37 +0700

    clean code roadmap

---
+++ src/docs/RoadMapData.jsx
@@ -0,0 +1,38 @@
+export const milestones = [
+    {
+      year: 2022,
+      title: "Penerapan Drone Reboisasi",
+      description:
+        "Teknologi drone digunakan untuk menanam pohon otomatis di area yang terdampak deforestasi.",
+      image: "/roadmap/drone.webp",
+    },
+    {
+      year: 2025,
+      title: "50% Energi Global dari Sumber Terbarukan",
+      description:
+        "Setengah dari energi global dipasok oleh sumber energi terbarukan, mengurangi ketergantungan pada bahan bakar fosil.",
+      image: "/roadmap/turbin.jpg",
+    },
+    {
+      year: 2030,
+      title: "Mobil Listrik Menguasai Pasar",
+      description:
+        "Penerimaan mobil listrik yang masif di seluruh dunia, mengurangi emisi karbon secara signifikan.",
+      image: "/roadmap/mobil.webp",
+    },
+    {
+      year: 2035,
+      title: "Penghentian Total Penggunaan Plastik Sekali Pakai",
+      description:
+        "Pencapaian penghentian penggunaan plastik sekali pakai di seluruh dunia, menggantinya dengan bahan ramah lingkungan.",
+      image: "/roadmap/plastik.jpg",
+    },
+    {
+      year: 2040,
+      title: "Hutan Buatan untuk Mengatasi Deforestasi",
+      description:
+        "Penerapan hutan buatan skala besar untuk membantu pemulihan ekosistem yang rusak.",
+      image: "/roadmap/hutan.jpg",
+    },
+  ];
+  
--- src/pages/Home/RoadMap.jsx
+++ src/pages/Home/RoadMap.jsx
@@ -1,44 +1,6 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion"; // Import AnimatePresence for exit animations
-
-const milestones = [
-  {
-    year: 2022,
-    title: "Penerapan Drone Reboisasi",
-    description:
-      "Teknologi drone digunakan untuk menanam pohon otomatis di area yang terdampak deforestasi.",
-    image: "/roadmap/drone.webp",
-  },
-  {
-    year: 2025,
-    title: "50% Energi Global dari Sumber Terbarukan",
-    description:
-      "Setengah dari energi global dipasok oleh sumber energi terbarukan, mengurangi ketergantungan pada bahan bakar fosil.",
-    image: "/roadmap/turbin.jpg",
-  },
-  {
-    year: 2030,
-    title: "Mobil Listrik Menguasai Pasar",
-    description:
-      "Penerimaan mobil listrik yang masif di seluruh dunia, mengurangi emisi karbon secara signifikan.",
-    image: "/roadmap/mobil.webp",
-  },
-  {
-    year: 2035,
-    title: "Penghentian Total Penggunaan Plastik Sekali Pakai",
-    description:
-      "Pencapaian penghentian penggunaan plastik sekali pakai di seluruh dunia, menggantinya dengan bahan ramah lingkungan.",
-    image: "/roadmap/plastik.jpg",
-  },
-  {
-    year: 2040,
-    title: "Hutan Buatan untuk Mengatasi Deforestasi",
-    description:
-      "Penerapan hutan buatan skala besar untuk membantu pemulihan ekosistem yang rusak.",
-    image: "/roadmap/hutan.jpg",
-  },
-];
-
+import { motion, AnimatePresence } from "framer-motion"; 
+import { milestones } from "../../docs/RoadMapData";
 const Roadmap = () => {
   const [selectedMilestoneIndex, setSelectedMilestoneIndex] = useState(0);
 
@@ -104,7 +66,7 @@ const Roadmap = () => {
                       : "bg-gray-100 border-4 border-primary-200"
                   } rounded-full flex items-center justify-center shadow-md transform transition-transform`}
                 >
-                  <span className=" font-bold">{milestone.year}</span>
+                  <span className="font-bold">{milestone.year}</span>
                 </div>
 
                 {/* Line Connector for Mobile */}
